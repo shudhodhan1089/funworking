2a717641276b3086392d288e6a48a91fdb763602
shudhodhan1089 <shudhodhanj2004@gmail.com>
2025-09-01 18:08:04 +0530

    Adding initial files needed

---
+++ learningnew.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    cout<<"Printing the table of 5"<<endl;
+    for(int i=1;i<=10;i++){
+        cout<<5<<" x "<<i<<" = "<<n*i<<endl;
+    }
+    return 0;
+}
